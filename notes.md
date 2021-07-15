3e77b84fbe2c31c76329bd1d88b765ef004ddd7f
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-07-15 13:51:41 -0400

    Make the NewPlantForm into a controlled component

--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
-# DELIVERABLE 1
+# DELIVERABLE 1: When the app starts, I can see all plants.
 
 ## Step 1:
   - [x] Initialize a state to hold the list of plants
@@ -8,4 +8,11 @@
   - [x] Pass the state of plants through to the plant list as a prop
   - [x] (In the PlantList component) Iterate through the plants, and render one PlantCard for each plant
 
+
+# DELIVERABLE 2: I can add a new plant to the page by submitting the form.
+
+## Step 1: Make the NewPlantForm into a controlled component
+  - [x] Need a formData state to hold the data
+  - [x] Connect the formData state to the inputs (make the input value the corresponding formData attribute)
+  - [x] Need onChange event handlers to update the state as inputs are changed
 
--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewPlantForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: "",
+  })
+
+  const handleChange = (event) => {
+    event.preventDefault()
+
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+        <input
+            name="name"
+            onChange={handleChange}
+            placeholder="Plant name"
+            type="text"
+            value={formData.name}
+        />
+        <input
+            name="image"
+            onChange={handleChange}
+            placeholder="Image URL"
+            type="text"
+            value={formData.image}
+        />
+        <input
+            name="price"
+            onChange={handleChange}
+            placeholder="Price"
+            step="0.01"
+            type="number"
+            value={formData.price}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
