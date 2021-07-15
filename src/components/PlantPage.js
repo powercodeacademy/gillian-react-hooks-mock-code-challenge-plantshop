ffc32772a02424b04d99d26084b5c679bba84ad1
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-07-15 13:23:12 -0400

    Got initial fetch for plants working

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]) // initialize a state to hold the list of plants
+
+  // use fetch to make a GET request for the plants and update the state with the response
+  // do all this inside of a useEffect
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(response => response.json())
+      .then(data => setPlants(data))
+  }, [])
+
+
   return (
     <main>
       <NewPlantForm />
